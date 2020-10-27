7bd8c5509d398ac4a341e65d789ea95912eaf7c4
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2020-10-27 20:04:06 +0100

    update label extension

--- Sources/UISugar/Extensions/UILabel + Extension.swift
+++ Sources/UISugar/Extensions/UILabel + Extension.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-extension UILabel {
+public extension UILabel {
     
     /// Creates a `UILabel` with the given text.
     static func withText(text: String?) -> UILabel {
@@ -29,5 +29,26 @@ extension UILabel {
             documentAttributes: nil)
         
         self.attributedText = attrStr
+    }
+    
+    func setLineSpacing(lineSpacing: CGFloat = 0.0, lineHeightMultiple: CGFloat = 0.0) {
+
+        guard let labelText = self.text else { return }
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = lineSpacing
+        paragraphStyle.lineHeightMultiple = lineHeightMultiple
+
+        let attributedString:NSMutableAttributedString
+        if let labelattributedText = self.attributedText {
+            attributedString = NSMutableAttributedString(attributedString: labelattributedText)
+        } else {
+            attributedString = NSMutableAttributedString(string: labelText)
+        }
+
+        // Line spacing attribute
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, attributedString.length))
+
+        self.attributedText = attributedString
     }
 }
