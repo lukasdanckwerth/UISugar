6e3dd0c070a53a12a233c785b5f5135218d6aa49
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2023-02-26 15:17:40 +0100

    CreatedCartsController empty state and remove all (#11)

--- Sources/UISugar/ViewControllers/UIImageViewController.swift
+++ Sources/UISugar/ViewControllers/UIImageViewController.swift
@@ -7,61 +7,86 @@
 #if os(iOS)
 import UIKit
 
-class UIImageViewController: UIViewController {
-   
-   
-   // MARK: - Properties
-   
-   /// The underlying image view of this image view controller.
-   open lazy var imageView = UIImageView()
-   
-   
-   // MARK: - Initialization
-   
-   /// Initialization with given image.
-   init(image: UIImage?) {
-      super.init(nibName: nil, bundle: nil)
-      imageView.image = image
-   }
-   
-   /// Initialization from storyboard.
-   required init?(coder aDecoder: NSCoder) {
-      super.init(coder: aDecoder)
-   }
-   
-   /// Initialization from given image name.
-   convenience init(imageName name: String?) {
-      self.init(image: name != nil ? UIImage(named: name!) : nil)
-   }
-   
-   
-   // MARK: - Override UIViewController
-   
-   override func loadView() {
-      imageView.alpha = 0
-      imageView.contentMode = .scaleAspectFill
-      view = imageView
-   }
-   
-   override func viewWillAppear(_ animated: Bool) {
-      super.viewWillAppear(animated)
-      
-      alphaAnimation(to: 1)
-   }
-   
-   override func viewWillDisappear(_ animated: Bool) {
-      super.viewWillDisappear(animated)
-      
-      alphaAnimation(to: 0)
-   }
-   
-   private func alphaAnimation(to alpha: CGFloat) {
-      
-      guard imageView.alpha != alpha else { return }
-      
-      UIView.animate(withDuration: UISugar.fadeDuration, animations: {
-         self.imageView.alpha = alpha
-      })
-   }
+open class UIImageViewController: UIViewController {
+    
+    public static let fadeInDuration: TimeInterval = 0.5
+    public static let fadeOutDuration: TimeInterval = 0.3
+    
+    
+    // MARK: - Properties
+    
+    /// The underlying image view of this image view controller.
+    ///
+    open lazy var imageView = UIImageView()
+    
+    
+    // MARK: - Initialization
+    
+    /// Initialization with given image.
+    ///
+    public init(image: UIImage?) {
+        super.init(nibName: nil, bundle: nil)
+        imageView.image = image
+        setup()
+    }
+    
+    /// Initialization from storyboard.
+    ///
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    /// Initialization from given image name.
+    ///
+    convenience public init(imageName name: String?) {
+        self.init(image: name != nil ? UIImage(named: name!) : nil)
+    }
+    
+    private func setup() {
+        imageView.alpha = 0
+        imageView.contentMode = .scaleAspectFill
+    }
+    
+    
+    // MARK: - Override UIViewController
+    
+    open override func loadView() {
+        view = UIView()
+        if #available(iOS 13.0, *) {
+            view.backgroundColor = .systemBackground
+        } else {
+            view.backgroundColor = .white
+        }
+        imageView.bind(to: view)
+    }
+    
+    open override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if animated {
+            alphaAnimation(to: 1, duration: UIImageViewController.fadeInDuration)
+        } else {
+            imageView.alpha = 1
+        }
+    }
+    
+    open override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if animated {
+            alphaAnimation(to: 0, duration: UIImageViewController.fadeOutDuration)
+        } else {
+            imageView.alpha = 0
+        }
+    }
+    
+    private func alphaAnimation(to alpha: CGFloat, duration: TimeInterval) {
+        guard imageView.alpha != alpha else { return }
+        
+        UIView.animate(withDuration: duration, animations: {
+            self.imageView.alpha = alpha
+        })
+    }
 }
 #endif
