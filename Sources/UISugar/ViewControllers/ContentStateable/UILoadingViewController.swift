618720cbb6c7763ab06ae13581b3df7910128b5f
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2023-05-03 22:08:09 +0200

    [TASK] update

--- Sources/UISugar/ViewControllers/ContentStateable/UILoadingViewController.swift
+++ Sources/UISugar/ViewControllers/ContentStateable/UILoadingViewController.swift
@@ -14,8 +14,6 @@ public class UIActivityPanel: UIVisualEffectView {
     
     // MARK: - Properties
     
-    
-    
     public let blurEffect = UIBlurEffect(style: .light)
     
     public let vibrancyEffect: UIVibrancyEffect
@@ -35,9 +33,11 @@ public class UIActivityPanel: UIVisualEffectView {
     
     
     /// The label displays the loading text.
+    ///
     public let label = UILabel()
     
     /// The label displays details text.
+    ///
     public let detailLabel = UILabel()
     
     
@@ -45,7 +45,6 @@ public class UIActivityPanel: UIVisualEffectView {
     // MARK: - Initialize
     
     init() {
-        
         self.vibrancyEffect = UIVibrancyEffect(blurEffect: blurEffect)
         self.vibrancyEffectView = UIVisualEffectView(effect: vibrancyEffect)
         
@@ -75,15 +74,11 @@ public class UIActivityPanel: UIVisualEffectView {
         stackView.addArrangedSubview(label)
         stackView.addArrangedSubview(detailLabel)
         
-        
-        // configure stack view
         stackView.axis = .vertical
         stackView.spacing = 8
         stackView.alignment = .center
         stackView.distribution = .fill
         
-        
-        // configure activity indicator view
         #if swift(>=4.2)
         activityIndicator.style = .whiteLarge
         #else
@@ -92,49 +87,26 @@ public class UIActivityPanel: UIVisualEffectView {
         activityIndicator.hidesWhenStopped = true
         activityIndicator.startAnimating()
         
-        
-        // configure label
         label.font = UIFont.preferredFont(forTextStyle: .title2)
         label.textAlignment = .center
         label.numberOfLines = 0
-        label.text = NSLocalizedString("Some Loading Text", comment: "")
         
-        
-        // configure detail label
         detailLabel.font = UIFont.preferredFont(forTextStyle: .body)
         detailLabel.textAlignment = .center
         detailLabel.numberOfLines = 0
-        detailLabel.text = NSLocalizedString("Some details text for the current task", comment: "")
-        
-        
-        //      contentView.clipsToBounds = false
-        //      contentView.layer.cornerRadius = UISugar.defaultCornerRadius
-        //      contentView.layer.masksToBounds = true
-        //      contentView.layer.shadowColor = UIColor.red.cgColor
-        //      contentView.layer.drawShadow()
         
         clipsToBounds = false
-        
         layer.cornerRadius = UISugar.defaultCornerRadius
         layer.masksToBounds = true
-        
     }
-    
-    /// Updates the label and text appereance dependand on text.  Label will be hidden if text is `nil`.  Text will be
-    /// capitalized if property `capitalizeText` is set to `true`.
-    //   private func updateLabel() {
-    //      label.isHidden = text == nil
-    //      label.text = capitalizeText ? text?.uppercased() : text
-    //   }
-    
 }
 
 @available(iOS 13.0, *)
-class UIBackgroundImageLoadingViewController: UIImageViewController {
+public class UIBackgroundImageLoadingViewController: UIImageViewController {
     
     lazy var activityPanel = UIActivityPanel()
     
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
         
         imageView.addSubview(activityPanel)
@@ -145,14 +117,17 @@ class UIBackgroundImageLoadingViewController: UIImageViewController {
 }
 
 @available(iOS 13.0, *)
-class UILoadingViewController: UIViewController {
+public class UILoadingViewController: UIViewController {
     
     /// The stack view wich contains the activity indicator and a label.
     @available(iOS 13.0, *)
-    lazy var stackView = UIActivityIndicatorStackView()
+    public lazy var stackView = UIActivityPanel()
     
     /// A Boolean value indicating whether the view is presented with a delay or instantly.  (Delay is `0.1` sec)
-    var isDelayed = true
+    ///
+    public var isDelayed = false
+    
+    public var delay: CGFloat = 0
     
     
     
@@ -162,43 +137,62 @@ class UILoadingViewController: UIViewController {
     ///
     /// - parameter message:    A message to be presented under the activity indicator.
     /// - parameter withDelay:  A Boolean value indicating whether to present the view instantly or after a delay.  (Delay is `0.1` sec)
-    convenience init(message: String? = nil, withDelay delay: Bool = true) {
-        self.init(nibName: nil, bundle: nil)
-        stackView.text = message
+    public init(message: String? = nil, withDelay delay: Bool = true) {
+        super.init(nibName: nil, bundle: nil)
+//        self.init(nibName: nil, bundle: nil)
+        //        stackView.text = message
         isDelayed = delay
+        setup()
+    }
+    
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+        setup()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    
+    private func setup() {
+        self.modalPresentationStyle = .overCurrentContext
+        self.modalTransitionStyle = .crossDissolve
     }
     
     
     
     // MARK: - Override UIViewController
     
-    override func loadView() {
+    public override func loadView() {
         view = UINotTappableView()
     }
     
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
         
         
         stackView.isHidden = true
-        stackView.capitalizeText = false
+//        stackView.capitalizeText = false
         stackView.alpha = 0
-        stackView.alignment = .center
-        stackView.distribution = .fillEqually
+//        stackView.alignment = .center
+//        stackView.distribution = .fillEqually
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(stackView)
         view.addConstraints([
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.widthAnchor.constraint(equalToConstant: 200)
         ])
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+    public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // We use a 0.1 second delay to not show an activity indicator in case the data loads very quickly.
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { [weak self] in
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
             UIView.animate(withDuration: UISugar.fadeDuration, animations: {
                 self?.stackView.isHidden = false
                 self?.stackView.alpha = 1
@@ -206,13 +200,13 @@ class UILoadingViewController: UIViewController {
         }
     }
     
-    override func viewDidLayoutSubviews() {
+    public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         stackView.sizeToFit()
     }
     
     #if swift(>=4.2)
-    override func didMove(toParent parent: UIViewController?) {
+    public override func didMove(toParent parent: UIViewController?) {
         super.didMove(toParent: parent)
         
         if let parentFrame = parent?.view.frame {
